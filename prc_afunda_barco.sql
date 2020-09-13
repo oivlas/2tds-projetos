be0ad60bcc706fb043c8f8298d6f814e21f05645
Gustavo Padlipskas <gpadlipskas@gmail.com>
2020-09-13 15:43:27 -0300

    import inicial para o github

---
+++ prc_afunda_barco.sql
@@ -0,0 +1,20 @@
+create or replace PROCEDURE PRC_AFUNDA_BARCO AS 
+v_vl_total number(30,2) := 00;
+v_vl_tot_item number(30,2) := 00;
+BEGIN
+  for c_x in ( select * from loc_item_locacao)
+  loop
+
+         for c_ped_loc in 
+         ( 
+         select cd_cliente, cd_func 
+         from   loc_item_locacao, loc_cliente, loc_funcionario
+         where  nr_pedido = c_x.nr_pedido
+         )
+         loop
+             v_vl_tot_item := v_vl_tot_item + 1;             
+         end loop;
+         v_vl_tot_item := 00;
+  
+  end loop;  
+END PRC_AFUNDA_BARCO;
